48b1af7cd997ae4525805418638c97f90a520314
Jessica JnoBaptiste <86270295+jessjno@users.noreply.github.com>
2024-10-14 19:46:34 +0000

    added default private to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_14_170250) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_14_192928) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -61,7 +61,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_14_170250) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "username"
-    t.boolean "private"
+    t.boolean "private", default: true
     t.integer "likes_count", default: 0
     t.integer "comments_count", default: 0
     t.datetime "created_at", null: false
