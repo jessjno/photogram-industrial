8a7cb3f7c46fb14cd94777eedb468cee88699598
Jessica JnoBaptiste <86270295+jessjno@users.noreply.github.com>
2024-10-15 15:07:45 +0000

    Added navbar and CDN assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
+  root "photos#index"
+  
+  devise_for :users
+
   resources :likes
   resources :follow_requests
-
   resources :photos
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "photos#index"
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
